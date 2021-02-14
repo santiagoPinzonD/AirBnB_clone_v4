5884d7da08ac0d07e39a903589074553e2014c1d
santiagopinzonD <santiagopinzon456@gmail.com>
2021-02-14 00:24:29 +0000

    fix semistandar

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,18 @@
-$(document).ready( function () {
-  let listCheck = []
-  $(".checkbox_amenities").click( function () {
-    let space = " ";
-    let string = space.concat('', $(this).attr("data-name")); 
-    if ($(this).is(':checked')){
+const $ = window.$;
+$(document).ready(function () {
+  const listCheck = [];
+  $('.checkbox_amenities').click(function () {
+    const space = ' ';
+    const string = space.concat('', $(this).attr('data-name'));
+    if ($(this).is(':checked')) {
       listCheck.push(string);
     } else {
-	let removeItemFromArr = ( listCheck, item ) => {
-		    let i = listCheck.indexOf( item );
-		        i !== -1 && listCheck.splice( i, 1 );
-	};
-	removeItemFromArr(listCheck, string);
+      const removeItemFromArr = (listCheck, item) => {
+        const i = listCheck.indexOf(item);
+        i !== -1 && listCheck.splice(i, 1);
+      };
+      removeItemFromArr(listCheck, string);
     }
-    $("#h4_amenities").text(listCheck);
-    });
+    $('#h4_amenities').text(listCheck);
+  });
 });
